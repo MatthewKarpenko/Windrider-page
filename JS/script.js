d2fa881958a2e8443e4fec76d1a04b60bc4d5ffa
Матвей Карпенко <mtw.karp@gmail.com>
2018-11-01 22:34:31 +0100

    master

--- JS/script.js
+++ JS/script.js
@@ -52,3 +52,67 @@ menuImage.addEventListener("click",moveMenu);
 
 	}
 }, 500);
+	
+let proriderInfoBlock = document.getElementById("smallDevInfo");
+let infoBlock = document.getElementById("hiddenInfoForSmallDev");
+let details = document.getElementById("detailsOfProrider");
+let proRiderImage = document.getElementsByClassName("mainPhotoOfProrider");
+let proRiderLinks = document.getElementsByClassName("proridersSocialLinksB");
+let procent = 10;
+let inputField = document.getElementById("postText");
+
+
+if (proriderInfoBlock) {
+
+proriderInfoBlock.addEventListener("click",openInfo);
+	function openInfo(event) {if (infoBlock.classList.contains("openMenu")){
+		infoBlock.style.opacity = "1";
+		details.classList.add("rotateItem");
+		infoBlock.classList.remove("hidden");
+		infoBlock.classList.remove("openMenu");
+	}else{
+		details.classList.remove("rotateItem");
+		infoBlock.classList.add("openMenu")
+		infoBlock.classList.add("hidden")
+		infoBlock.style.opacity = "0"
+
+	} 
+
+	}
+
+inputField.addEventListener("focus",function(event){
+	if (this.value === "Что у вас нового ?"){
+		this.value = "";
+	}
+	
+
+})
+inputField.addEventListener("focusout",function(event){
+	if (this.value === ""){
+	this.value = "Что у вас нового ?"
+}
+})
+
+}
+
+
+
+/*
+ let intervalAnimation = setInterval(function linkAnimation() {
+		if (procent === -3) {
+			cleanInterval();
+		}else{
+		proRiderLinks.style.paddingLeft = procent + "%"
+		procent - 1;
+	}
+
+	},100);
+
+	
+	
+	function cleanInterval() {
+		clearInterval(intervalAnimation)
+	};
+
+}
+*/
